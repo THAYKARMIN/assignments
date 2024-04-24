cf84e3550826fb7f20b877deb55af7fcef6e4915
Thay Karmin <thay.karmin@weizmann.ac.il>
2024-04-24 14:13:35 +0300

    final fix

--- day02/guess_game.py
+++ day02/guess_game.py
@@ -7,11 +7,10 @@ count = 0
 while guess != number:
     if guess > number:
         print("your guess is too big, try again")
-        count += 1
     elif guess < number:
         print("your guess is too small, try again")
-        count += 1
+    count += 1
     guess = int(input("guess again: "))
-    
+
 print("congratulation, your guess is correct!")
 print(f"It took you only {count} guesse to get there!")
